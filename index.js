48a372cc4d3a07ff960ac804fe7bbe7773c1469f
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-16 17:55:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
+
+const num1 = 31;
+const num2 = 2;
+const num3 = 34;
+const num4 = 15;
+
+const multiply = num1*num2;
+const mod = num3%num4;
+let random = parseInt(Math.floor(Math.random()*10));
+const max =Math.max(10,15,20);
+
 // Write your code here
